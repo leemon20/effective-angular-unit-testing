ee91271debb6adff41c3db66c1f9b1b444062753
Dmitrij Vojtov <dmitrij.vojtov@compeople.de>
2025-06-04 20:09:33 +0200

    initial

---
+++ src/app/parent-component/parent-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parent-component',
+  imports: [],
+  templateUrl: './parent-component.component.html',
+  styleUrl: './parent-component.component.scss'
+})
+export class ParentComponentComponent {
+
+}
